7ccee0ec6453cd9f00b8ed410e30d2848b14b93e
IzzyDaQueen <izzybetan@gmail.com>
2019-05-12 13:02:28 +0800

    added user nteraction!!

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -15,6 +15,7 @@ class Boot extends Scene {
         this.load.image("bk1", "./src/assets/bk1.png")
         this.load.image("avatar1", "./src/assets/avatar1.png")
         this.load.image("enemy1", "./src/assets/enemy1.png")
+        this.load.atlasXML("rts","./src/assets/kenney_rtssci-fi/Spritesheet/scifiRTS_spritesheet.png","./src/assets/kenney_rtssci-fi/Spritesheet/scifiRTS_spritesheet.xml")      
     }
 
     create() {
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -17,11 +17,18 @@ class Game extends Scene {
     }
 
     create() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+
         this.background = this.add.image(0, 0,"bk1")
         
         this.background.setOrigin(0,0)
         this.background.setDisplaySize(800,600)
 
+        this.char = this.add.sprite(50,50,"rts","scifiUnit_01.png")
+        this.add.sprite(100,100,"rts","scifiStructure_13.png")
+        this.add.sprite(50,50,"rts", "scifiUnit_01.png")
+        this.add.sprite(150,150,"rts", "scifiUnit_02.png")
+     
         this.spriteGroup = this.add.group()
 
         for ( let i = 0; i < 10; i++){
@@ -30,8 +37,6 @@ class Game extends Scene {
                 x: i*30,
                 y: 500,
                 texture: "avatar1" 
-                
-    
             }))
 
         }
@@ -44,10 +49,21 @@ class Game extends Scene {
     }
 
     update(time) {
-        let sprites = this.spriteGroup.children.entries
-        for( let i = 0; i < sprites.length; i++){
-            sprites[i].update(time)
+        
+        if(this.cursors.left.isDown){
+            this.char.x -= 5
         }
+        if(this.cursors.right.isDown){
+            this.char.x += 5
+        }
+        if(this.cursors.up.isDown){
+            this.char.y -= 5
+        }
+        if(this.cursors.down.isDown){
+            this.char.y += 5
+        }
+        
+        
     }
 
 
